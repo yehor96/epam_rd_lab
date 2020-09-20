06f17f3d228fad89ab0423e7e730b7639e603e5b
Yehor Shyriaiev <yehor_shyriaiev@epam.com>
2020-09-20 17:10:24 +0300

    Modify ht14 to Selenide

--- src/test/java/com/epam/test/ht14/SeleniumTest.java
+++ src/test/java/com/epam/test/ht14/SeleniumTest.java
@@ -1,56 +1,48 @@
 package com.epam.test.ht14;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SeleniumTest {
+    @RegisterExtension
+    static ScreenShooterExtension screenshotAll = new ScreenShooterExtension(true);
+
     private static final String SEARCH_FIELD = ".gLFyf.gsfi";
     private static final String PAGE_LINKS = ".TbwUpd.NJjxre";
+    private static final String BANNER_TEXT = ".hero.homepage h1";
 
     private static final String SEARCH_QUERY = "selenium";
     private static final String WEBSITE_URL = "www.selenium.dev";
-    private static final String BASE_URL = "https://google.com";
     private static final String EXPECTED_TEXT = "Selenium automates browsers. That's it!";
-    private static final String BANNER_TEXT = ".hero.homepage h1";
+    private static final String BROWSER = "opera";
 
-    private static WebDriver driver;
-
-    @BeforeEach
-    void setUp(){
-        WebDriverManager.chromedriver().browserVersion("85").setup();
-        driver = new ChromeDriver();
+    @BeforeAll
+    static void setUpAll(){
+        Configurator.setUp(BROWSER);
     }
 
     @Test
-    void keywordSearch(){
-        driver.get(BASE_URL);
-
-        WebElement SearchFieldElement = driver.findElement(By.cssSelector(SEARCH_FIELD));
-        SearchFieldElement.sendKeys(SEARCH_QUERY);
-        SearchFieldElement.submit();
-
-        driver.findElements(By.cssSelector(PAGE_LINKS))
+    void testKeywordSearch(){
+        Selenide.open("/");
+        $(SEARCH_FIELD).setValue(SEARCH_QUERY).submit();
+        $$(PAGE_LINKS)
                 .stream()
                 .filter(each -> each.getText().equals(WEBSITE_URL))
                 .findFirst().get().click();
-
-        String actualUrl = driver.getCurrentUrl();
-        String actualText = driver.findElement(By.cssSelector(BANNER_TEXT)).getText();
-
-        assertTrue(actualUrl.contains(SEARCH_QUERY));
-        assertTrue(actualText.equals(EXPECTED_TEXT));
+        assertTrue(WebDriverRunner.url().contains(SEARCH_QUERY));
+        assertTrue($(BANNER_TEXT).getText().equals(EXPECTED_TEXT));
     }
 
     @AfterEach
     void destroy(){
-        driver.close();
+        WebDriverRunner.closeWindow();
     }
 }
