7848e36c66a7a97e5794f153adaeb051b6ef8c14
Yehor Shyriaiev <yehor_shyriaiev@epam.com>
2020-09-23 12:09:22 +0300

    Modify ht18

--- src/test/java/com/epam/test/ht14/SelenideTest.java
+++ src/test/java/com/epam/test/ht14/SelenideTest.java
@@ -3,15 +3,15 @@ package com.epam.test.ht14;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.junit5.ScreenShooterExtension;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SeleniumTest {
+public class SelenideTest {
     @RegisterExtension
     static ScreenShooterExtension screenshotAll = new ScreenShooterExtension(true);
 
@@ -31,18 +31,18 @@ public class SeleniumTest {
 
     @Test
     void testKeywordSearch(){
+        //GIVEN
         Selenide.open("/");
+
+        //WHEN
         $(SEARCH_FIELD).setValue(SEARCH_QUERY).submit();
         $$(PAGE_LINKS)
                 .stream()
                 .filter(each -> each.getText().equals(WEBSITE_URL))
                 .findFirst().get().click();
+
+        //THEN
         assertTrue(WebDriverRunner.url().contains(SEARCH_QUERY));
         assertTrue($(BANNER_TEXT).getText().equals(EXPECTED_TEXT));
-    }
-
-    @AfterEach
-    void destroy(){
-        WebDriverRunner.closeWindow();
     }
 }
